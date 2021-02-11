aeac7aef2fab397c66dabeab86cee96d0600e9d1
ADMINRG-KAOAAHK\New user <jamesoermann@yahoo.com>
2021-02-10 18:32:24 -0600

    Homework for week 2

---
+++ Week2Homework/Weektwopizzaplace.js
@@ -0,0 +1,12 @@
+
+let pizzaPlace = "Honkey Tonk Pizza"; 
+let numberOfToppings = 10;
+console.log (pizzaPlace, typeof pizzaPlace  );
+console.log (numberOfToppings, typeof numberOfToppings);
+console.log (`${pizzaPlace} is a great place. You can get ${numberOfToppings} different toppings there.`);
+if (numberOfToppings < 10 ) {
+    console.log("Quality, not quantity.");
+  } else {
+    console.log("A whole lot of pizza.");
+  }
+
