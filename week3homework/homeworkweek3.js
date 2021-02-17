c45beeb946bce0766642aa75fca0d5654e312ebb
ADMINRG-KAOAAHK\New user <jamesoermann@yahoo.com>
2021-02-16 18:10:40 -0600

    add week three

---
+++ week3homework/homeworkweek3.js
@@ -0,0 +1,12 @@
+let pizzaToppings = ['mozzarella', 'provolone', 'pepperoni', 'onion', 'garlic'];
+function greetCustomer() {
+
+}
+let greetCustomer = "Welcome to Honkey Tonk Pizza. We have " loop pizzaToppings " "
+
+function getPizzaOrder(size, toppings, crust,){
+    print 
+
+}  
+let preparePizza = ['size', 'crust', 'listOfToppings'];
+function  
